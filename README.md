25f69968fed279c209e60200838c1947eb1a80b1
gtatiya <gtatiya@live.com>
2019-04-06 09:36:09 -0400

    arm + gripper MoveIt!

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Hacking-SotA-UR5
 
+## UR5 Single Arm + RobotiQ 85 gripper
+
+## Gazebo
+`roslaunch gazebo_ros empty_world.launch paused:=true use_sim_time:=false gui:=true throttled:=false recording:=false debug:=true` 
+`rosrun gazebo_ros spawn_model -file src/ur5_single_arm_tufts/urdf/ur5_single_arm.urdf -urdf -x 0 -y 0 -z 1 -model ur5_single_arm`
+
+### Gazebo + MoveIt Rviz:
+`roslaunch ur5_single_arm_tufts ur5_single_arm_gazebo.launch`
+
+### Launch Gazebo and execute trajectories:
+`roslaunch ur5_single_arm_manipulation execute_trajectory.launch`
+
+### Execute trajectories (open Gazebo first):
+`rosrun ur5_single_arm_ur5_manipulation execute_trajectory.py`
+
 ## UR5 Single Arm
 
 ### Gazebo + MoveIt Rviz:
@@ -11,7 +26,14 @@
 ### Execute trajectories (open Gazebo first):
 `rosrun ur5_single_arm_ur5_manipulation execute_trajectory.py`
 
-## Gazebo
-`roslaunch gazebo_ros empty_world.launch paused:=true use_sim_time:=false gui:=true throttled:=false recording:=false debug:=true` 
+## RobotiQ 85 gripper
 
-`rosrun gazebo_ros spawn_model -file src/ur5_single_arm_tufts/urdf/ur5_single_arm.urdf -urdf -x 0 -y 0 -z 1 -model ur5_single_arm`
+### Gazebo + MoveIt Rviz:
+`roslaunch ur5_single_arm_tufts ur5_single_arm_gazebo_gripper.launch`
+
+### Launch Gazebo and execute trajectories:
+`roslaunch ur5_single_arm_gripper_manipulation execute_trajectory.launch`
+
+### Execute trajectories (open Gazebo first):
+`rosrun ur5_single_arm_gripper_manipulation execute_trajectory.py`
+
---
+++ ur5_single_arm_manipulation/scripts/execute_trajectory.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import rospy
+import moveit_commander
+
+rospy.init_node('execute_trajectory', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+arm_group = moveit_commander.MoveGroupCommander("manipulator")
+grp_group = moveit_commander.MoveGroupCommander("gripper")
+
+# You can get the reference frame for a certain group by executing this line:
+print "Arm Reference frame: %s" % arm_group.get_planning_frame()
+print "Gripper Reference frame: %s" % grp_group.get_planning_frame()
+
+# You can get the end-effector link for a certaing group executing this line:
+print "Arm End effector: %s" % arm_group.get_end_effector_link()
+print "Gripper End effector: %s" % grp_group.get_end_effector_link()
+
+# You can get a list with all the groups of the robot like this:
+print "Robot Groups:"
+print robot.get_group_names()
+
+# You can get the current values of the joints like this:
+print "Arm Current Joint Values:"
+print arm_group.get_current_joint_values()
+print "Gripper Current Joint Values:"
+print grp_group.get_current_joint_values()
+
+# You can also get the current Pose of the end-effector of the robot like this:
+print "Arm Current Pose:"
+print arm_group.get_current_pose()
+
+# Finally, you can check the general status of the robot like this:
+print "Robot State:"
+print robot.get_current_state()
+
+
+arm_group.set_named_target('up')
+arm_group.go(wait=True)
+print("Point 1")
+
+arm_group.set_joint_value_target([-0.21957805043352518, -1.097296859939564, 1.8945345194815335,
+                            -2.366067038969164, -1.571228181260084, -1.0061550793898952])
+arm_group.go(wait=True)
+print("Point 2")
+
+# Close
+grp_group.set_joint_value_target([0.8039005131791948, -0.8039005131791948, 0.8039005131791948, 0.8039005131791948, -0.8039005131791948, 0.8039005131791948])
+grp_group.go(wait=True)
+print("Point 3")
+
+pose = arm_group.get_current_pose().pose
+pose.position.x += 0.1
+pose.position.y += 0.1
+pose.position.z += 0.1
+arm_group.set_pose_target(pose)
+arm_group.go(wait=True)
+print("Point 4")
+
+# Open
+grp_group.set_joint_value_target([9.800441184282249e-05, -9.800441184282249e-05, 9.800441184282249e-05, 9.800441184282249e-05, -9.800441184282249e-05, 9.800441184282249e-05])
+grp_group.go(wait=True)
+print("Point 5")
+
+arm_group.set_named_target('up')
+arm_group.go(wait=True)
+print("Point 6")
